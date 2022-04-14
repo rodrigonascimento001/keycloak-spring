686984390146cad68041d930a31e70640aa33a40
Rodrigo Nascimento <rodrigo_allexandre@yahoo.com.br>
2022-04-14 09:00:55 -0300

    adicionando configuracoes iniciais do projeto

---
+++ src/main/java/com/br/rodrigonasc/keycloakspring/KeycloakSpringApplication.java
@@ -0,0 +1,13 @@
+package com.br.rodrigonasc.keycloakspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/rodrigonasc/keycloakspring/KeycloakSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.rodrigonasc.keycloakspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
